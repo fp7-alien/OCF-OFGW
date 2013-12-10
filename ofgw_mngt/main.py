bdba8b904b1f63eef4a1e318d7470a3b5838bbb6
Krzysztof Dombek <krzysztof.dombek@gmail.com>
2013-12-10 13:54:12 +0100

    Initial dir structure

---
+++ ofgw_mngt/main.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python
+#-*- coding: utf-8 -*-
+"""
+OFGW Management Module
+This module is a part of OFGW that is an extension to
+OFELIA Control Framework. It contains CLI tool for providing management
+actions on non-OpenFlow capatible devices.
+"""
+
+def usage():
+	print ""
+
+if __name__ == '__main__':
+    usage();
