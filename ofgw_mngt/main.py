d30ededd4ba6401984e809c46b540f0563e606a8
Krzysztof Dombek <krzysztof.dombek@gmail.com>
2013-12-12 17:01:04 +0100

    Added CLI parser

--- ofgw_mngt/main.py
+++ ofgw_mngt/main.py
@@ -7,8 +7,23 @@ OFELIA Control Framework. It contains CLI tool for providing management
 actions on non-OpenFlow capatible devices.
 """
 
+import sys
+import argparse
+
 def usage():
-	print ""
+    print "USAGE: main.py <config_dir>"
+    sys.exit(1);
 
 if __name__ == '__main__':
-    usage();
+    # if len(sys.argv) < 1:
+    #     usage();
+    parser = argparse.ArgumentParser(description="OFGW management script")
+    parser.add_argument("command",
+                        choices=["list", "reboot", "reset"],
+                        help="Management action")
+    parser.add_argument("DEVICE_ID", help="Device identifier")
+    parser.add_argument("-s", "--status",
+                        help="Get device status",
+                        action="store_true")
+    args = parser.parse_args()
+    # print args.accumulate(args.integers)
