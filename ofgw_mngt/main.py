65a1dbb90601f49a807cfb79b054a4cf4397df04
Krzysztof Dombek <krzysztof.dombek@gmail.com>
2013-12-16 16:13:33 +0100

    Configuration parser added, changes in CLI params parser

--- ofgw_mngt/main.py
+++ ofgw_mngt/main.py
@@ -9,21 +9,59 @@ actions on non-OpenFlow capatible devices.
 
 import sys
 import argparse
+import yaml
+import ConfigParser
 
-def usage():
-    print "USAGE: main.py <config_dir>"
-    sys.exit(1);
+
+def parseConfig(inventory_conf="./inventory.ini", groups_conf="./groups.yaml"):
+    conf_invent = open(groups_conf, 'r')
+    print yaml.load(conf_invent)
+
+    # Config = MyParser()
+    parser = ConfigParser.ConfigParser()
+    parser.read(inventory_conf)
+    for section_name in parser.sections():
+        print 'Section:', section_name
+        print '  Options:', parser.options(section_name)
+        for name, value in parser.items(section_name):
+            print '  %s = %s' % (name, value)
+        print
 
 if __name__ == '__main__':
     # if len(sys.argv) < 1:
     #     usage();
+
     parser = argparse.ArgumentParser(description="OFGW management script")
+    # group = parser.add_mutually_exclusive_group()
+    # parser.add_argument("list",
+    #                     help="Get list of available devices",
+    #                     action="store_true")
+    # parser.add_argument("reboot",
+    #                     help="Reboot the selected device",
+    #                     action="store_true")
+    # parser.add_argument("reset",
+    #                     help="Restore the factory settings on device",
+    #                     action="store_true")
     parser.add_argument("command",
-                        choices=["list", "reboot", "reset"],
+                        choices=["list", "reboot", "reset", "config"],
                         help="Management action")
-    parser.add_argument("DEVICE_ID", help="Device identifier")
+    # parser.add_argument("DEVICE_ID",
+    #                     help="Device identifier",
+    #                     action="store_const")
     parser.add_argument("-s", "--status",
                         help="Get device status",
                         action="store_true")
     args = parser.parse_args()
+    command = args.command
+    
+    if command == "list":
+        print "Listing devices...\n"
+    elif command == "reboot":
+        print "Device reboot initialized...\n"
+    elif command == "reset":
+        print "Reset device to factory setting initialized...\n"
+    elif command == "config":
+        print "Parsing the configuration...\n"
+        parseConfig()
+
     # print args.accumulate(args.integers)
---
+++ ofgw_mngt/test/test_main.py
@@ -0,0 +1,15 @@
+import unittest
+
+class ConfigurationTests(unittest.TestCase):
+
+    def testGroups(self):
+        self.failUnless(False)
+
+    def testInventory(self):
+        self.failUnless(False)
+
+def main():
+    unittest.main()
+
+if __name__ == '__main__':
+    main()
